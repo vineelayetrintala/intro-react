f01f7e36126e445d0ab6ce26d83450707f9cc6ed
Vineela Yetrintala <Vineela.yetrintala@gmail.com>
2020-07-16 13:08:56 -0500

    uncomment assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -49,15 +49,7 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
-      tabChoice = (
-        <List
-          placeholder="Add Assignment..."
-          currList={this.state.assignments}
-          addFunction={this.addAssignment}
-          title="Assignments"
-        />
-      );
+    
     }*/
 
     /* Change below to render students*/
